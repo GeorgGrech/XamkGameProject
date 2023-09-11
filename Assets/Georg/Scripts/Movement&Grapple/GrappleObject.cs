dbd436caa422ee66383ae52ac2105c90549b9111
GeorgGrech <fgegr001@edu.xamk.fi>
2023-09-11 20:08:29 +0300

    Player grapple
    Created functioning grapple mechanic

---
+++ Assets/Georg/Scripts/Movement&Grapple/GrappleObject.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleObject : MonoBehaviour
+{
+    public Vector3 grapplePoint;
+    public PlayerGrapple playerGrapple;
+    public float grappleSpeed;
+
+    public string grappleableLayer;
+
+    private bool hasConnected = false;
+
+    public Coroutine grappleCoroutine; //Used for both throwing grapple and pulling player
+
+    [SerializeField] private GameObject stopperPrefab;
+    private GameObject stopper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision) //Collision with actual surfaces
+    {
+        if (!hasConnected) //Grapple hasn't connected to anything yet
+        {
+            if(LayerMask.LayerToName(collision.gameObject.layer) == grappleableLayer) //If collision is grappleable environment
+            {
+                Debug.Log("Connected to Grappleable surface");
+                hasConnected = true;
+
+                GetComponent<Rigidbody>().isKinematic = true;
+                StopCoroutine(grappleCoroutine); //Stop grapple move towards target
+
+                grappleCoroutine = StartCoroutine(playerGrapple.PullPlayer()); //Start pulling player
+            }
+            else //If collision isn't grappleable environment
+            {
+                Debug.Log("Hit non-Grappleable surface");
+                //Cancel functionality
+                CancelGrapple();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GrappleStopper"))
+        {
+            Debug.Log("No surface hit.");
+            CancelGrapple();
+        }
+    }
+
+    public IEnumerator ThrowGrapple()
+    {
+        transform.LookAt(grapplePoint);
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, grapplePoint, grappleSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void ExecThrowGrapple(bool stopperNeeded)
+    {
+        if(stopperNeeded) stopper = Instantiate(stopperPrefab,grapplePoint, Quaternion.identity);
+
+        grappleCoroutine = StartCoroutine(ThrowGrapple());
+    }
+
+    public void CancelGrapple()
+    {
+        if (grappleCoroutine != null)
+        {
+            StopCoroutine(grappleCoroutine);
+        }
+
+        if (stopper != null)
+        {
+            Destroy(stopper);
+        }
+
+        playerGrapple.grappling = false;
+        Destroy(gameObject);
+    }
+}
--- Assets/Georg/Scripts/Movement&Grapple/PlayerGrapple.cs
+++ Assets/Georg/Scripts/Movement&Grapple/PlayerGrapple.cs
@@ -2,69 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerGrapple : MonoBehaviour
 {
-    public Transform playerCam;
-    public GameObject grapplePrefab;
+    [SerializeField] private Transform playerCam;
+    [SerializeField] private GameObject grapplePrefab;
 
-    bool grappling = false;
+    public bool grappling = false;
 
+    [SerializeField] private Transform throwPoint;
+    [SerializeField] private float maxGrappleDistance;
+    [SerializeField] private float pullForce;
 
     [Header("Instantiated Grapple Properties")]
-    public float grappleSpeed;
+    [SerializeField] private float grappleSpeed;
     private Vector3 grapplePoint;
     public LayerMask grappleableLayer;
+    public string grappleableLayerName; //Using LayerMask with the grappleObject coughed up errors. So string it is. Cry about it.
 
-
+    private GrappleObject grappleObject;
 
     [Header("Input")]
     public KeyCode grappleKey = KeyCode.Mouse1;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(grappleKey)) StartGrapple();
+        if (Input.GetKeyDown(grappleKey) && !grappling) StartGrapple();
+
+        if (Input.GetKeyUp(grappleKey) && grappling) grappleObject.CancelGrapple();
     }
 
     private void StartGrapple()
     {
+        bool stopperNeeded;
+        Debug.Log("StartGrapple");
         grappling = true;
 
         RaycastHit hit;
-        if (Physics.Raycast(playerCam.position, playerCam.forward, out hit, /*maxGrappleDistance,*/ grappleableLayer))
+        if (Physics.Raycast(playerCam.position, playerCam.forward, out hit, maxGrappleDistance, grappleableLayer))
         {
             grapplePoint = hit.point;
+            stopperNeeded = false;
             
         }
         else
         {
-            grapplePoint = playerCam.position + playerCam.forward /** maxGrappleDistance*/;
+            grapplePoint = playerCam.position + playerCam.forward * maxGrappleDistance;
+            stopperNeeded = true;
         }
 
+        InstantiateGrapple(stopperNeeded);
 
         //lr.enabled = true;
         //lr.SetPosition(1, grapplePoint);
     }
 
-    private IEnumerator ThrowGrapple()
+    private void InstantiateGrapple(bool stopperNeeded)
     {
-        Quaternion camRotation = playerCam.rotation;
-        yield return null;
-    }
-
-    private void InstantiateGrapple()
-    {
-        GrappleObject grappleObject = GameObject.Instantiate(grapplePrefab).GetComponent<GrappleObject>();
+        grappleObject = GameObject.Instantiate(grapplePrefab,throwPoint.position,Quaternion.identity).GetComponent<GrappleObject>();
 
         grappleObject.grappleSpeed = grappleSpeed;
         grappleObject.grapplePoint = grapplePoint;
+        grappleObject.playerGrapple = this;
+        grappleObject.grappleableLayer = grappleableLayerName;
 
-        grappleObject.ExecMoveToGrapple();
+        grappleObject.ExecThrowGrapple(stopperNeeded);
+    }
+
+    public IEnumerator PullPlayer()
+    {
+        Debug.Log("Pulling player");
+        Vector3 dir = (grapplePoint - transform.position).normalized;
+        while (true)
+        {
+            rb.AddForce(dir * pullForce); //Keep adding force
+            yield return null;
+        }
     }
 }
