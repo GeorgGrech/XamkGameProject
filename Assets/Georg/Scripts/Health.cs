cf8f15e58207bc61990e5a7a9ccae336e4a08883
SamuelFenech <Samuel.Fenech.d57070@mcast.edu.mt>
2023-12-12 23:51:47 +0200

    Player Can Lose

--- Assets/Georg/Scripts/Health.cs
+++ Assets/Georg/Scripts/Health.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 /// <summary>
@@ -62,6 +63,11 @@ public class Health : MonoBehaviour
         
         gameObject.SendMessageUpwards("OnDeath", SendMessageOptions.DontRequireReceiver);
 
+        if(CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+
+        }
         //Destroy(gameObject);
         //StartCoroutine(DestroyObject(this.gameObject));
     }
