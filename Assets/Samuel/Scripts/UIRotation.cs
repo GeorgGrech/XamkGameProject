1aa09b7248a630a2748a164df7920f9d4a7e0f17
GeorgGrech <fgegr001@edu.xamk.fi>
2023-12-12 19:37:23 +0200

    UI fix and misc changes for player

--- Assets/Samuel/Scripts/UIRotation.cs
+++ Assets/Samuel/Scripts/UIRotation.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class UIRotation : MonoBehaviour
 {
    //public GameObject canvas;
-   public Camera mainCamera;
+   private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
     void Update()
     {
-        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.up);
+        //transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.up);
+        transform.forward = mainCamera.transform.forward;
     }
 }
