7eb5c87e3d71ed8fc749ec1fc1681f17fefd42f3
AnderAzzopardi <56016944+AnderAzzopardi@users.noreply.github.com>
2023-12-12 14:35:23 +0200

    GameOverScene and Turret
    Finished GameOver Scene and changed the turret model

---
+++ Assets/AnderFolder/GameOver/BackToMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    // Add this script to your button GameObject
+
+    // Specify the scene name you want to go back to
+    public string SampleScene;
+
+    // Function to handle button click
+    public void OnBackButtonClick()
+    {
+        // Check if the target scene name is not empty
+        if (!string.IsNullOrEmpty(SampleScene))
+        {
+            // Load the target scene
+            SceneManager.LoadScene(SampleScene);
+        }
+        else
+        {
+            Debug.LogWarning("Target scene name is not set!");
+        }
+    }
+}
