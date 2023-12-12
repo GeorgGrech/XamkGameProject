d3536e6982dbacfcd7a93d9abf780c01f344c8f0
SamuelFenech <Samuel.Fenech.d57070@mcast.edu.mt>
2023-12-12 21:21:41 +0200

    TerrainBouncesPlayer

---
+++ Assets/Samuel/Scripts/BounceTerrain.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceTerrain : MonoBehaviour
+{
+    // Timer to track collision time
+	float _timeColliding;
+	// Time before damage is taken, 1 second default
+	public float timeThreshold = 1f;
+
+    public float launchForce;
+
+//    void OnCollisionEnter(Collision collider)
+//    {
+//     if(collider.gameObject.tag == "Player")
+//     {
+//         collider.gameObject.SendMessageUpwards("RespawnPlayer", SendMessageOptions.DontRequireReceiver);
+//     }
+//    }
+
+    public GameObject GlobalVolume;
+    void Start()
+    {
+        launchForce = 30f;
+        GlobalVolume = GameObject.Find("Global Volume");
+    }
+
+   void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			// _timeColliding = 0f;
+
+			// Debug.Log("Enemy started colliding with player.");
+
+			// collision.gameObject.SendMessageUpwards("ChangeHealth", -20f, SendMessageOptions.DontRequireReceiver);
+            // GlobalVolume.SendMessageUpwards("PlayDamageAnimation", SendMessageOptions.DontRequireReceiver);
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            if (playerRigidbody != null)
+            {
+                // Apply an upward force
+                Debug.Log("LaunchPlayer");
+                playerRigidbody.AddForce(Vector3.up * launchForce, ForceMode.Impulse);
+                collision.gameObject.SendMessageUpwards("ChangeHealth", -20f, SendMessageOptions.DontRequireReceiver);
+                GlobalVolume.SendMessageUpwards("PlayDamageAnimation", SendMessageOptions.DontRequireReceiver);
+            }
+		}
+    }
+//    void OnCollisionStay(Collision collision)
+//    {
+//         if(collision.gameObject.tag == "Player")
+//         {
+//             if (_timeColliding < timeThreshold) {
+//                     _timeColliding += Time.deltaTime;
+//                 } else {
+//                     // Time is over theshold, player takes damage
+//                     collision.gameObject.SendMessageUpwards("ChangeHealth", -20f, SendMessageOptions.DontRequireReceiver);
+//                     GlobalVolume.SendMessageUpwards("PlayDamageAnimation", SendMessageOptions.DontRequireReceiver);
+//                     // Reset timer
+//                     _timeColliding = 0f;
+//                 }
+//         }
+//    }
+
+}
